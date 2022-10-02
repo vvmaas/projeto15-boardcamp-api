6d3ace62494f26bebb3187bb120357a9dac440d3
Victor Vermaas <vhvermaas@gmail.com>
2022-10-02 10:11:36 -0300

    ending rental function

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -9,19 +9,19 @@ async function listRentals(req,res) {
     try {
         if (gameId && customerId){
             rentals = await connection.query(
-                'SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "customerId" = $1 AND "gameId" = $2;',
+                `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "customerId" = $1 AND gameId" = $2;`,
                 [customerId, gameId])
         } else if (gameId) {
             rentals = await connection.query(
-                'SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "gameId" = $1;',
+                `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "gameId" = $1;`,
                 [gameId])
         } else if (customerId) {
             rentals = await connection.query(
-                'SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "customerId" = $1;',
+                `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId" WHERE "customerId" = $1;`,
                 [customerId])
         } else {
             rentals = await connection.query(
-                'SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId";'
+                `SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", categories.name AS "categoryName" FROM rentals JOIN customers ON customers.id=rentals."customerId" JOIN games ON games.id=rentals."gameId" JOIN categories ON categories.id=games."categoryId";`
                 )
         }
         
@@ -58,24 +58,50 @@ async function postRental(req,res){
 }
 
 async function endRental(req,res){
+    const id = req.params.id
+    const rentEnd = new Date('2022-11-02')
+
+    const validId = await connection.query(`SELECT * FROM rentals WHERE id=$1`, [id]) 
+    if(!validId.rows[0]){
+        return res.sendStatus(404)
+    }
+    if(validId.rows[0].returnDate !== null){
+        return res.sendStatus(400)
+    }
+    console.log(new Date())
+    const dayPrice = validId.rows[0].originalPrice / validId.rows[0].daysRented
+    let difference = rentEnd.getTime() - validId.rows[0].rentDate.getTime()
+    let daysDelayed = Math.ceil(difference / (1000 * 3600 * 24)) - validId.rows[0].daysRented
+
+    const delayFee = daysDelayed * dayPrice
+
+    try {
+        await connection.query(`UPDATE rentals SET "returnDate"=$1 WHERE id=$2`, [rentEnd, id])
+        await connection.query(`UPDATE rentals SET "delayFee"=$1 WHERE id=$2`, [delayFee, id])
+        
+        res.sendStatus(200)
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500)
+    }
 
 }
 
 async function deleteRental(req,res) {
     const id = req.params.id
     try {
-        const target = await connection.query(`SELECT * FROM rentals WHERE id=${id}`)
+        const target = await connection.query(`SELECT * FROM rentals WHERE id=$1`, [id])
         if(!target.rows[0]){
             return res.sendStatus(404)
         }
         if(target.rows[0].returnDate === null){
             return res.sendStatus(400)
         }
-        await connection.query(`DELETE FROM rentals WHERE id=${id}`)
+        await connection.query(`DELETE FROM rentals WHERE id=$1`, [id])
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
     }
 }
 
-export {listRentals, postRental, deleteRental}
+export {listRentals, postRental, deleteRental, endRental}
--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { listRentals, postRental, deleteRental } from "../controllers/rentals.controller.js";
+import { listRentals, postRental, deleteRental, endRental } from "../controllers/rentals.controller.js";
 
 const rentalsRouter = Router()
 
 rentalsRouter.get('/rentals', listRentals)
 rentalsRouter.post('/rentals', postRental)
-rentalsRouter.post('/rentals/:id/return')
+rentalsRouter.post('/rentals/:id/return', endRental)
 rentalsRouter.delete('/rentals/:id', deleteRental)
 
 export default rentalsRouter
